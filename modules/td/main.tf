ae5d3a129940e81ce488f228cde12e633249102d
nisali <nisalikularatne@gmail.com>
2023-01-17 22:11:35 -0500

    modify td

--- modules/td/main.tf
+++ modules/td/main.tf
@@ -2,7 +2,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_iam_role" "labrole" {
-  name = "AWSServiceRoleForECS"
+  name = "FullAccessRole"
 }
 
 data "terraform_remote_state" "this" {
