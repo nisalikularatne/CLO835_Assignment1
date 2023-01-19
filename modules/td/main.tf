69a49e6189d5c116353f36a5b0adaa5f6598b753
nisali <nisalikularatne@gmail.com>
2023-01-19 09:41:27 -0500

    connfigruing db host

--- modules/td/main.tf
+++ modules/td/main.tf
@@ -40,7 +40,7 @@ resource "aws_ecs_task_definition" "application" {
       hostPort      = var.host_port
     }]
     environment = [
-      { "name" : "DBHOST", "value" : "10.0.5.155" },
+      { "name" : "DBHOST", "value" : "127.0.0.1" },
       { "name" : "DBPORT", "value" : "3306" },
       { "name" : "DBUSER", "value" : "root" },
       { "name" : "DATABASE", "value" : "employees" },
